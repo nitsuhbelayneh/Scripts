7d87215f0f908d571051caa03be90be926439577
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-03-25 15:56:22 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,7 +6,14 @@ sudo apt update
 # Upgrade installed packages
 sudo apt-get dist-upgrade -y
 
+#install qemu guest agent and restart it (so the machine better comunicate with proxmox)
+sudo apt install qemu-guest-agent -y
+sudo systemctl restart qemu-guest-agent
 
+#install openshh-server and configure it so that root can ssh as well
+sudo apt install openssh-server
+sed -i '/#PermitRootLogin prohibit-password/i PermitRootLogin yes' /etc/ssh/sshd_config
+sudo systemctl restart ssh 
 
 # Delete the script file
 rm "$0"
