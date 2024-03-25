d49f80c78e04e595f71c6229de2ff989222ac460
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-03-25 16:12:48 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -11,9 +11,20 @@ sudo apt install qemu-guest-agent -y
 sudo systemctl restart qemu-guest-agent
 
 #install openshh-server and configure it so that root can ssh as well
-sudo apt install openssh-server
+sudo apt install openssh-server -y
 sed -i '/#PermitRootLogin prohibit-password/i PermitRootLogin yes' /etc/ssh/sshd_config
 sudo systemctl restart ssh 
 
+# Remove and clear
+sudo apt autoremove
+history -c
+
 # Delete the script file
 rm "$0"
+
+#for desktop servers to enable rdp un comment the lines below
+
+# sudo apt install xserver-xorg-core
+# sudo apt install xorgxrdp
+# sudo apt install xrdp
+
