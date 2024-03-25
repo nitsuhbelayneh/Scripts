c5f775903ee3294f88de47210d7782e35eb7bcdf
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-03-25 17:19:38 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -15,10 +15,6 @@ sudo apt install openssh-server -y
 sed -i '/#PermitRootLogin prohibit-password/i PermitRootLogin yes' /etc/ssh/sshd_config
 sudo systemctl restart ssh 
 
-# Remove and clear
-sudo apt autoremove
-history -c
-
 
 
 #for desktop servers to enable rdp un comment the lines below
@@ -27,6 +23,23 @@ history -c
 # sudo apt install xorgxrdp
 # sudo apt install xrdp
 
+# Check if the lines are already present in the file /etc/gdm3/custom.conf
+if grep -q "AutomaticLoginEnable=True" /etc/gdm3/custom.conf && grep -q "AutomaticLogin=root" /etc/gdm3/custom.conf && grep -q "AllowRoot=True" /etc/gdm3/custom.conf; then
+  echo "Lines already exist in /etc/gdm3/custom.conf"
+else
+  # Add the lines under [daemon]
+  sed -i '/\[daemon\]/a AutomaticLoginEnable=True\nAutomaticLogin=root' /etc/gdm3/custom.conf
+
+  # Add the line AllowRoot=True below # TimedLoginDelay = 10
+  sed -i '/#  TimedLoginDelay = 10/a AllowRoot=True' /etc/gdm3/custom.conf
+
+  echo "Lines added to /etc/gdm3/custom.conf"
+fi
+
+# Comment out the line in /etc/pam.d/gdm-password
+sed -i 's/^auth required     pam_succeed_if.so user != root quiet/#&/' /etc/pam.d/gdm-password
+
+echo "Line commented in /etc/pam.d/gdm-password"
 
 
 
@@ -37,9 +50,9 @@ history -c
 
 
 
-
-
-
+# Remove and clear
+sudo apt autoremove
+history -c
 
 # Delete the script file
 rm "$0"
