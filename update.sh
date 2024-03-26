2f50a0a83d2a31068dfb816a4c2d3edb627dd7ae
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-03-26 11:17:05 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -14,9 +14,9 @@ sudo apt install qemu-guest-agent -y
 sudo systemctl restart qemu-guest-agent
 
 #install openshh-server and configure it so that root can ssh as well
-sudo apt install openssh-server -y
-sed -i '/#PermitRootLogin prohibit-password/i PermitRootLogin yes' /etc/ssh/sshd_config
-sudo systemctl restart ssh 
+#sudo apt install openssh-server -y
+#sed -i '/#PermitRootLogin prohibit-password/i PermitRootLogin yes' /etc/ssh/sshd_config
+#sudo systemctl restart ssh 
 
 
 
@@ -27,9 +27,9 @@ sudo apt install xorgxrdp -y
 sudo apt install xrdp -y
 
 # Check if the lines are already present in the file /etc/gdm3/custom.conf
-#if grep -q "AutomaticLoginEnable=True" /etc/gdm3/custom.conf && grep -q "AutomaticLogin=root" /etc/gdm3/custom.conf && grep -q "AllowRoot=True" /etc/gdm3/custom.conf; then
-#  echo "Lines already exist in /etc/gdm3/custom.conf"
-#else
+if grep -q "AutomaticLoginEnable=True" /etc/gdm3/custom.conf && grep -q "AutomaticLogin=root" /etc/gdm3/custom.conf && grep -q "AllowRoot=True" /etc/gdm3/custom.conf; then
+  echo "Lines already exist in /etc/gdm3/custom.conf"
+else
   # Add the lines under [daemon]
   sed -i '/\[daemon\]/a AutomaticLoginEnable=True\nAutomaticLogin=root' /etc/gdm3/custom.conf
 
@@ -37,7 +37,7 @@ sudo apt install xrdp -y
   sed -i '/#  TimedLoginDelay = 10/a AllowRoot=True' /etc/gdm3/custom.conf
 
   #echo "Lines added to /etc/gdm3/custom.conf"
-#fi
+fi
 
 # Comment out the line in /etc/pam.d/gdm-password
 #sed -i 's/^auth required    pam_succeed_if.so user != root quiet/#&/' /etc/pam.d/gdm-password
