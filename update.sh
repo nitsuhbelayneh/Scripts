5bf5f5e68576ee35e1e8ce92e4e0be161b481a43
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-03-26 22:17:35 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -37,10 +37,12 @@ sudo systemctl restart ssh
 
 
 
+
+
 # Check if "AutomaticLoginEnable=True" exists in /etc/gdm3/custom.conf
-if grep -q "^\s*AutomaticLoginEnable=True" /etc/gdm3/custom.conf; then
+if grep -qE "^\s*AutomaticLoginEnable=True" /etc/gdm3/custom.conf; then
   # Check if the line is commented
-  if ! grep -q "^#.*AutomaticLoginEnable=True" /etc/gdm3/custom.conf; then
+  if ! grep -qE "^#.*AutomaticLoginEnable=True" /etc/gdm3/custom.conf; then
     # Add a new line above the commented line
     echo "Line 'AutomaticLoginEnable=True' already exists in custom.conf"
     #sed -i '/^#.*AutomaticLoginEnable=True/i AutomaticLoginEnable=True' /etc/gdm3/custom.conf
