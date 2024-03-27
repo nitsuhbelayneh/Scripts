0fe140b6d7db3bd6543dee3096758deec7d20250
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-03-27 09:53:44 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -39,12 +39,13 @@ if grep -q "^\s*PermitRootLogin\s+yes" /etc/ssh/sshd_config; then
   echo "The line 'PermitRootLogin yes' already exists and is uncommented in /etc/ssh/sshd_config"
 else
   if grep -q "^\s*PermitRootLogin\s+yes" /etc/ssh/sshd_config && grep -q "^\s*#\s*PermitRootLogin\s+yes" /etc/ssh/sshd_config; then
-    sed -i '/^#PermitRootLogin\s+prohibit-password/i PermitRootLogin yes' /etc/ssh/sshd_config
+    #sed -i '/^#PermitRootLogin\s+prohibit-password/i PermitRootLogin yes' /etc/ssh/sshd_config
     #sed -i '/#PermitRootLogin prohibit-password/i PermitRootLogin yes' /etc/ssh/sshd_config
-    #sed -i '/^\s*#\s*PermitRootLogin\s+yes/s/^#\s*//' /etc/ssh/sshd_config
+    sed -i '/^\s*#\s*PermitRootLogin\s+yes/s/^#\s*//' /etc/ssh/sshd_config
     echo "Line 'PermitRootLogin yes' was commented but is now uncommented in /etc/ssh/sshd_config"
   else
-    sed -i '/^#PermitRootLogin\s+prohibit-password/i PermitRootLogin yes' /etc/ssh/sshd_config
+    sed -i '/^\s*#\s*PermitRootLogin\s+yes/s/^#\s*//' /etc/ssh/sshd_config
+    #sed -i '/^#PermitRootLogin\s+prohibit-password/i PermitRootLogin yes' /etc/ssh/sshd_config
     echo "Added line 'PermitRootLogin yes' above '#PermitRootLogin prohibit-password' in /etc/ssh/sshd_config"
   fi
 fi
