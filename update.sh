302d60faeb691fff809c8ecd9cc567654dc0edf5
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-03-27 07:39:49 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -17,16 +17,16 @@
 # use the same formating as the below for the error that reapets the line when it is commeted and without comment, use an and operator and chek them both at the same time
 
 # Check if "PermitRootLogin yes" exists and is commented in /etc/ssh/sshd_config
-if grep -q "#\s*PermitRootLogin\s+yes" /etc/ssh/sshd_config && ! grep -q "^\s*PermitRootLogin\s+yes" /etc/ssh/sshd_config; then
-  # Add a new line above "#PermitRootLogin prohibit-password"
-  #sed -i '/#PermitRootLogin prohibit-password/i PermitRootLogin yes' /etc/ssh/sshd_config
-  echo " Line 'PermitRootLogin yes' exist in /etc/ssh/sshd_config"
-else
-  sed -i '/#PermitRootLogin prohibit-password/i PermitRootLogin yes' /etc/ssh/sshd_config
-  echo "Added line 'PermitRootLogin yes' above '#PermitRootLogin prohibit-password' in /etc/ssh/sshd_config"
-fi
+#if grep -q "#\s*PermitRootLogin\s+yes" /etc/ssh/sshd_config && ! grep -q "^\s*PermitRootLogin\s+yes" /etc/ssh/sshd_config; then
+#  # Add a new line above "#PermitRootLogin prohibit-password"
+#  #sed -i '/#PermitRootLogin prohibit-password/i PermitRootLogin yes' /etc/ssh/sshd_config
+#  echo " Line 'PermitRootLogin yes' exist in /etc/ssh/sshd_config"
+#else
+#  sed -i '/#PermitRootLogin prohibit-password/i PermitRootLogin yes' /etc/ssh/sshd_config
+#  echo "Added line 'PermitRootLogin yes' above '#PermitRootLogin prohibit-password' in /etc/ssh/sshd_config"
+#fi
 
-sudo systemctl restart ssh 
+#sudo systemctl restart ssh 
 
 
 #for desktop servers to enable rdp un comment the lines below
@@ -39,40 +39,40 @@ sudo systemctl restart ssh
 
 
 
-# Check if "AutomaticLoginEnable=True" exists in /etc/gdm3/custom.conf
-if grep -qE "^\s*AutomaticLoginEnable=True" /etc/gdm3/custom.conf; then
-  # Check if the line is commented
-  if ! grep -qE "^#.*AutomaticLoginEnable=True" /etc/gdm3/custom.conf; then
-    # Add a new line above the commented line
-    echo "Line 'AutomaticLoginEnable=True' already exists in custom.conf"
-    #sed -i '/^#.*AutomaticLoginEnable=True/i AutomaticLoginEnable=True' /etc/gdm3/custom.conf
-    
-  else
-    sed -i '/\[daemon\]/a AutomaticLoginEnable=True' /etc/gdm3/custom.conf
-    echo "Added a new line 'AutomaticLoginEnable=True' above the commented line in custom.conf"
-  fi
-else
-  # Add the line under [daemon]
-  sed -i '/\[daemon\]/a AutomaticLoginEnable=True' /etc/gdm3/custom.conf
-  echo "Added line 'AutomaticLoginEnable=True' under [daemon] in custom.conf"
-fi
-
-# Check if "AutomaticLogin=root" exists in /etc/gdm3/custom.conf
-if grep -q "AutomaticLogin=root" /etc/gdm3/custom.conf; then
-  # Check if the line is commented
-  if grep -q "^#.*AutomaticLogin=root" /etc/gdm3/custom.conf; then
-    # Add a new line above the commented line
-    sed -i '/\[daemon\]/a AutomaticLogin=root' /etc/gdm3/custom.conf
-    #sed -i '/^#.*AutomaticLogin=root/i AutomaticLogin=root' /etc/gdm3/custom.conf
-    echo "Added a new line 'AutomaticLogin=root' above the commented line in custom.conf"
-  else
-    echo "Line 'AutomaticLogin=root' already exists in custom.conf"
-  fi
-else
-  # Add the line under [daemon]
-  sed -i '/\[daemon\]/a AutomaticLogin=root' /etc/gdm3/custom.conf
-  echo "Added line 'AutomaticLogin=root' under [daemon] in custom.conf"
-fi
+## Check if "AutomaticLoginEnable=True" exists in /etc/gdm3/custom.conf
+#if grep -qE "^\s*AutomaticLoginEnable=True" /etc/gdm3/custom.conf; then
+#  # Check if the line is commented
+#  if ! grep -qE "^#.*AutomaticLoginEnable=True" /etc/gdm3/custom.conf; then
+#    # Add a new line above the commented line
+#    echo "Line 'AutomaticLoginEnable=True' already exists in custom.conf"
+#    #sed -i '/^#.*AutomaticLoginEnable=True/i AutomaticLoginEnable=True' /etc/gdm3/custom.conf
+#    
+#  else
+#    sed -i '/\[daemon\]/a AutomaticLoginEnable=True' /etc/gdm3/custom.conf
+#    echo "Added a new line 'AutomaticLoginEnable=True' above the commented line in custom.conf"
+#  fi
+#else
+#  # Add the line under [daemon]
+#  sed -i '/\[daemon\]/a AutomaticLoginEnable=True' /etc/gdm3/custom.conf
+#  echo "Added line 'AutomaticLoginEnable=True' under [daemon] in custom.conf"
+#fi
+#
+## Check if "AutomaticLogin=root" exists in /etc/gdm3/custom.conf
+#if grep -q "AutomaticLogin=root" /etc/gdm3/custom.conf; then
+#  # Check if the line is commented
+#  if grep -q "^#.*AutomaticLogin=root" /etc/gdm3/custom.conf; then
+#    # Add a new line above the commented line
+#    sed -i '/\[daemon\]/a AutomaticLogin=root' /etc/gdm3/custom.conf
+#    #sed -i '/^#.*AutomaticLogin=root/i AutomaticLogin=root' /etc/gdm3/custom.conf
+#    echo "Added a new line 'AutomaticLogin=root' above the commented line in custom.conf"
+#  else
+#    echo "Line 'AutomaticLogin=root' already exists in custom.conf"
+#  fi
+#else
+#  # Add the line under [daemon]
+#  sed -i '/\[daemon\]/a AutomaticLogin=root' /etc/gdm3/custom.conf
+#  echo "Added line 'AutomaticLogin=root' under [daemon] in custom.conf"
+#fi
 
 ###################################################################################################################
 
@@ -104,9 +104,15 @@ fi
 
 
 # Check if "AllowRoot=True" exists and is commented in /etc/gdm3/custom.conf
-if ! grep -q "#\s*AllowRoot=True" /etc/gdm3/custom.conf && grep -q "^\s*AllowRoot=True" /etc/gdm3/custom.conf; then
-  echo "Line 'AllowRoot=True' is already exists in /etc/gdm3/custom.conf"
-  
+#if grep -q "^\s*AllowRoot=True" /etc/gdm3/custom.conf; then
+if grep -q "AllowRoot=True" /etc/gdm3/custom.conf; then
+  #echo "The line "AllowRoot=True" alrady exists in /etc/gdm3/custom.conf"
+  if grep -q "#\s*AllowRoot=True" /etc/gdm3/custom.conf; then 
+    sed -i '/#\s*TimedLoginDelay = 10/a AllowRoot=True' /etc/gdm3/custom.conf
+    echo "Line 'AllowRoot=True' was commented but now addes to /etc/gdm3/custom.conf"
+  else
+    echo "The line "AllowRoot=True" alrady exists in /etc/gdm3/custom.conf"
+  fi
 else
   # Add a new line above "# TimedLoginDelay = 10"
   sed -i '/#\s*TimedLoginDelay = 10/a AllowRoot=True' /etc/gdm3/custom.conf
@@ -118,20 +124,20 @@ fi
 
 ##############################################################################################################################
 
-# Check if the line "auth required pam_succeed_if.so user != root quite_success" is already commented in /etc/pam.d/gdm-password
-if grep -q "^#.*auth\s\+required\s\+pam_succeed_if\.so\s\+user\s\+!=\s\+root\s\+quiet" /etc/pam.d/gdm-password; then
-  echo "Line 'auth required pam_succeed_if.so user != root quite_success' is already commented in gdm-password"
-else
-  # Add a comment above the line
-  sed -i '/auth\s\+required\s\+pam_succeed_if\.so\s\+user\s\+!=\s\+root\s\+quiet/s/^/# /' /etc/pam.d/gdm-password
-  echo "Added a comment above the line 'auth required pam_succeed_if.so user != root quite_success' in gdm-password"
-fi
+## Check if the line "auth required pam_succeed_if.so user != root quite_success" is already commented in /etc/pam.d/gdm-password
+#if grep -q "^#.*auth\s\+required\s\+pam_succeed_if\.so\s\+user\s\+!=\s\+root\s\+quiet" /etc/pam.d/gdm-password; then
+#  echo "Line 'auth required pam_succeed_if.so user != root quite_success' is already commented in gdm-password"
+#else
+#  # Add a comment above the line
+#  sed -i '/auth\s\+required\s\+pam_succeed_if\.so\s\+user\s\+!=\s\+root\s\+quiet/s/^/# /' /etc/pam.d/gdm-password
+#  echo "Added a comment above the line 'auth required pam_succeed_if.so user != root quite_success' in gdm-password"
+#fi
 
 
 
 
 # Restart the rdp service after changing the configuration files
-sudo systemctl restart xrdp
+#sudo systemctl restart xrdp
 
 
 # Remove and clear
