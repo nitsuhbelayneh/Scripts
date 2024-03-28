cf145488b1e10cc166d29339ac43ffe353f799c9
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-03-28 15:57:15 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,53 @@
 #!/bin/bash
 
+
+
+##################################################################################################
+
+
+
+# Define the netplan configuration template
+netplan_config="network:
+  version: 2
+  renderer: NetworkManager
+  ethernets:
+    ens18:
+      addresses:
+      - 196.189.155.143/27
+      routes:
+      - to: 0.0.0.0/0
+        via: 196.189.155.129
+      nameservers:
+        addresses:
+        - 10.3.1.5 
+        - 8.8.8.8
+    ens19:
+      addresses:
+      - 172.16.143.50/29
+      routes:
+      - to: 10.190.10.16/32
+        via: 172.16.143.49
+      - to: 10.204.182.92/32
+        via: 172.16.143.49
+      - to: 10.175.206.40/29
+        via: 172.16.143.49"
+
+# Define the netplan configuration file path
+netplan_file="/etc/netplan/new" #test file
+#netplan_file="/etc/netplan/new/01-network-manager-all.yaml" #Real file
+
+# Write the netplan configuration to the file
+echo "$netplan_config" | sudo tee "$netplan_file" > /dev/null
+
+# Apply the netplan configuration
+#sudo netplan apply
+
+
+
+
+
+################################################################################################
+
 <<comment
 # Update the package lists
 sudo apt update
@@ -104,22 +152,18 @@ sudo systemctl restart xrdp
 
 comment
 
+
+<<comment
 # Remove residual packages
 sudo apt autoremove -y
 
 #Clear command history 
 history -c
 
+comment
+
 #Remove the bash history
-#sudo cat /dev/null > .bash_history
-#sudo echo -n > .bash_history
 sudo truncate -s 0 .bash_history
-
-
-#sudo rm .bash_history
-#sudo touch .bash_history
-
-
 
 # Delete the script file
 rm "$0"
