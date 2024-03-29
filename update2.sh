8c97a9fb7738fb2b676af36c0f2285072aff7b96
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-03-29 10:07:17 +0300

    Update update2.sh

--- update2.sh
+++ update2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#<<comment
 
 # Define the netplan configuration template
 netplan_config="network:
@@ -27,10 +27,31 @@ netplan_config="network:
         via: 172.16.143.49"
 
 # Define the netplan configuration file path
-netplan_file="/etc/netplan/new/01-network-manager-all.yaml"
+netplan_file="/etc/netplan/new"
+#netplan_file="/etc/netplan/01-network-manager-all.yaml"
 
-# Write the netplan configuration to the file
-echo "$netplan_config" | sudo tee "$netplan_file" > /dev/null
+
+# Check if the netplan configuration file already exists
+if [[ -f "$netplan_file" ]]; then
+  # Check if there is existing configuration written
+  #Check for any thing that is written in the configuration file
+  if grep -qs '^' "$netplan_file"; then  
+  #Check only for the word network in the configuration file
+  #if grep -q 'network:' "$netplan_file"; then
+    # Comment out the existing configuration
+    sed -i 's/^/#/' "$netplan_file"
+    # Add the new configuration after commented lines
+    echo "$netplan_config" | sudo tee -a "$netplan_file" > /dev/null
+  else
+    # Write the netplan configuration to the file
+    echo "$netplan_config" | sudo tee "$netplan_file" > /dev/null
+  fi
+else
+  # Write the netplan configuration to the file
+  echo "The file $netplan_file does not exists"
+  #echo "$netplan_config" | sudo tee "$netplan_file" > /dev/null
+fi
+
 
 # Apply the netplan configuration
 sudo netplan apply
