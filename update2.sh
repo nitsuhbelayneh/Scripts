779eb5a33cd85c1b5fa1769817806818f7d6cf8e
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-04-01 13:54:41 +0300

    Update update2.sh

--- update2.sh
+++ update2.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-
-
-  
-#<<comment
-
 ############################################################################################################################################
 
 #add this to the main script (update.sh)
@@ -15,53 +10,24 @@ netplan_config="network:
   ethernets:
     ens18:
       addresses:
-      - 196.189.155.143/27
+      - 
       routes:
-      - to: 0.0.0.0/0
-        via: 196.189.155.129
+      - to: 
+        via: 
       nameservers:
         addresses:
-        - 10.3.1.5 
-        - 8.8.8.8
+        - 
+        - 
     ens19:
       addresses:
-      - 172.16.143.50/29
+      - 
       routes:
-      - to: 10.190.10.16/32
-        via: 172.16.143.49
-      - to: 10.204.182.92/32
-        via: 172.16.143.49
-      - to: 10.175.206.40/29
-        via: 172.16.143.49"
-
-############################################################################################################################################
-
-# Define the netplan configuration template
-netplan_config="network:
-  version: 2
-  renderer: NetworkManager
-  ethernets:
-    ens18:
-      addresses:
-      - 196.189.155.143/27 #Change This
-      routes:
-      - to: 0.0.0.0/0
-        via: 196.189.155.129 #Change This
-      nameservers:
-        addresses:
-        - 10.3.1.5 
-        - 8.8.8.8
-    ens19:
-      addresses:
-      - 172.16.143.50/29
-      routes:
-      - to: 10.190.10.16/32
-        via: 172.16.143.49 #Change This
-      - to: 10.204.182.92/32
-        via: 172.16.143.49 #Change This
-      - to: 10.175.206.40/29
-        via: 172.16.143.49" #Change This
-
+      - to: 
+        via: 
+      - to: 
+        via: 
+      - to: 
+        via: "
 # Define the netplan configuration file path
 netplan_file="/etc/netplan/new"
 #netplan_file="/etc/netplan/01-network-manager-all.yaml"
@@ -89,3 +55,5 @@ fi
 
 # Apply the netplan configuration
 sudo netplan apply
+
+############################################################################################################################################
