ebef8e947a0fe57d8b7d5afef4a4eab03c0ac160
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-04-03 17:01:28 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -247,16 +247,16 @@ comment
 # Install vscode
 
 sudo apt update
-sudo apt install software-properties-common apt-transport-https wget gpg
+sudo apt install software-properties-common apt-transport-https wget gpg -y
 
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/packages.microsoft.gpg -y
 
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 
 sudo apt update
 
-sudo apt install code
+sudo apt install code -y
 
 #comment
 
