f5ad276f560a0d431ae1ea41adda2f3c2754e697
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-04-03 16:55:01 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -240,6 +240,40 @@ sudo netplan apply
 
 comment
 
+############################################################################################################################################
+
+#<<comment
+
+# Install vscode
+
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget gpg
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/packages.microsoft.gpg
+
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt update
+
+sudo apt install code
+
+#comment
+
+############################################################################################################################################
+
+#<<comment
+
+# Install winbox
+
+sudo apt update
+sudo apt install snapd
+sudo snap install core
+sudo snap install winbox
+
+# Add the file config part
+
+#comment
 
 ############################################################################################################################################
 
