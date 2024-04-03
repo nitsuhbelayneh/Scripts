bfd9356e84c620ca5ec3ff43c4704261c39f1544
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-04-03 17:05:27 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -249,7 +249,7 @@ comment
 sudo apt update
 sudo apt install software-properties-common apt-transport-https wget gpg -y
 
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg -y
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/packages.microsoft.gpg -y
 
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
