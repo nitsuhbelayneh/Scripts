4d4d6f28e5d269c355019f6a5c163f8041c246c6
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-04-04 15:27:00 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -337,7 +337,7 @@ comment
 #<<comment
 
 # Step 1: Create the .desktop file
-#touch ~/.local/share/applications/winbox.desktop
+touch ~/.local/share/applications/winbox.desktop
 cat <<EOT > ~/.local/share/applications/winbox.desktop
 [Desktop Entry]
 Name=Winbox
