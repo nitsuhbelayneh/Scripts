05dc04bc3195f82f509e4e7d54279c7b9427e621
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-05-10 16:10:55 +0300

    .

--- update.sh
+++ update.sh
@@ -12,31 +12,31 @@ sudo apt-get dist-upgrade -y
 
 ############################################################################################################################################
 
-#<<comment
+<<comment
 
 #install qemu guest agent and restart it (so the machine better comunicate with proxmox)
 sudo apt install qemu-guest-agent -y
 sudo systemctl restart qemu-guest-agent
 
-#comment
+comment
 
 ############################################################################################################################################
 
-#<<comment
+<<comment
 
 #install openshh-server
 sudo apt install openssh-server -y
 
-#comment
+comment
 
 ############################################################################################################################################
 
-#<<comment
+<<comment
 
 #Restart the ssh Service
 sudo systemctl restart ssh 
 
-#comment
+comment
 
 ############################################################################################################################################
 
@@ -85,5 +85,5 @@ echo "Command history cleared."
 rm "$0"
 echo "Script file deleted."
 
-
-
+# Completely clear the command history
+history -c && history -w
