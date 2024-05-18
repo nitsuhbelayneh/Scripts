aa615f4b5cfc0eb381a8241d4296dffda262a50c
clean <105063598+nitsuhbelayneh@users.noreply.github.com>
2024-05-18 10:51:29 +0300

    .

--- update.sh
+++ update.sh
@@ -12,17 +12,17 @@ sudo apt-get dist-upgrade -y
 
 ############################################################################################################################################
 
-#<<comment
+<<comment
 
 #install qemu guest agent and restart it (so the machine better comunicate with proxmox)
 sudo apt install qemu-guest-agent -y
 sudo systemctl restart qemu-guest-agent
 
-#comment
+comment
 
 ############################################################################################################################################
 
-#<<comment
+<<comment
 
 #install openshh-server
 sudo apt install openssh-server -y
@@ -32,11 +32,11 @@ sudo apt install openssh-server -y
 #Restart the ssh Service
 sudo systemctl restart ssh 
 
-#comment
+comment
 
 ############################################################################################################################################
 
-#<<comment
+<<comment
 
 # Install xrdp for desktop servers
 
@@ -48,6 +48,22 @@ sudo apt install xrdp -y
 
 # Restart the rdp service after changing the configuration files
 sudo systemctl restart xrdp
+
+comment
+
+############################################################################################################################################
+
+#<<comment
+color_profile_file="/etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla"
+color_profile_file="/new/folder/new.pkla"
+color_profile="[Allow Colord all Users]
+Identity=unix-user:*
+Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
+ResultAny=no
+ResultInactive=no
+ResultActive=yes"
+
+echo "$color_profile" | sudo tee -a "$color_profile_file" > /dev/null
 
 #comment
 
